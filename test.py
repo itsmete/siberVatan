0ee8dfea3ed62d332e6783d47e49d285001f9e6c
itsmete <bymcvisuals@icloud.com>
2023-12-20 17:14:22 +0300

    first lesson with homework

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-print('itsmete comapnyyy')
+def alan_hesapla(taban,yukseklik):
+                  alan = (taban*yukseklik) / 2
+                  return alan
 
-#itsmete corporation
+taban = int(input('taban girin'))
+yukseklik = int(input('yukseklik girin'))
+
+result = alan_hesapla(taban=taban,yukseklik=yukseklik)
+
+print(result)
+
