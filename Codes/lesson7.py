de2947e3b2af22d6a12d2b87ac82720d32fe406a
itsmete <bymcvisuals@icloud.com>
2024-02-03 11:21:06 +0300

    start of lesson7

---
+++ Codes/lesson7.py
@@ -0,0 +1,18 @@
+a,b,c,d = 50,100,50,75
+
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+
+username1 = "mete"
+username2 = "alparslan"
+
+
+result =  username1 == "mete"
+print(result)
+result =  username2 == "mete"
+print(result)
